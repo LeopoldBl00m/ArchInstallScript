8d1c0c6924a60295010c7665f11190f983e1648c
LeopoldBl00m <91829475+LeopoldBl00m@users.noreply.github.com>
2021-11-07 20:12:28 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+# == MY ARCH INSTALL SCRIPT == #
+#part1
+echo "Welcome to LeopoldBl00m's Arch Install script"
+echo "This install script basically only works for my system, so use it at your own risk!"
+echo "Please read the 'script.sh' to make sure that it will work for your system too."
+read -p "Are you ready to start the script? [y/n]" answer
+if [[ $answer = n ]] ; then
+echo "Take as much time as you need"
+echo "This script is intended for people with good knowledge of how to install arch systems to use"
+echo "If you are new to installing arch linux, I will reccomend watching 'DenshiVideo's video called 'Arch Linux: A Comfy Install Guide' to get a basic understanding of how to install arch and linux in general."
+if [[ $answer = y ]] ; hen
+echo "Let us go ahead with the install!"
+echo "Hopefully I programmed it right so it doesn't mess up your system, but, you were warned ;)"
