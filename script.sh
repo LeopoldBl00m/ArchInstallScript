33b7d884e04738b11100044ccf84380b63a57fa9
LeopoldBl00m <91829475+LeopoldBl00m@users.noreply.github.com>
2021-11-12 16:33:21 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,6 +8,6 @@ if [[ $answer = n ]] ; then
 echo "Take as much time as you need"
 echo "This script is intended for people with good knowledge of how to install arch systems to use"
 echo "If you are new to installing arch linux, I will reccomend watching 'DenshiVideo's video called 'Arch Linux: A Comfy Install Guide' to get a basic understanding of how to install arch and linux in general."
-if [[ $answer = y ]] ; hen
+if [[ $answer = y ]] ; then
 echo "Let us go ahead with the install!"
 echo "Hopefully I programmed it right so it doesn't mess up your system, but, you were warned ;)"
